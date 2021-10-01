46a00e23aeccc78e34b6d58c3dedea6ef85c67ea
pooya <info@centurytrail.com>
2021-10-01 10:17:07 -0700

    Initial Commit

---
+++ SwiftUIListAndNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListAndNavigation
+//
+//  Created by Pooya on 2021-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListAndNavigation/SwiftUIListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListAndNavigationApp.swift
+//  SwiftUIListAndNavigation
+//
+//  Created by Pooya on 2021-10-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
